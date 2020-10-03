65896e76b2d8880065d59818921ebbcd7aba1ee5
Fabian Delgado <nfabian.1231@gmail.com>
2020-10-03 00:07:16 -0500

    Version withe cleanest pom

--- src/test/java/com/testSuiteMonefy/runnerTestAddIncome.java
+++ src/test/java/com/testSuiteMonefy/runnerTestAddIncome.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (features = "src/features/home.feature", tags="@Alltestcases") 
+@CucumberOptions (features = "src/features/home.feature", tags="@Addincomes") 
 public class runnerTestAddIncome 
 {
 	
